7ca5e44936577fe757416b51665ab70dc84baa63
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-04-12 04:18:32 +0700

    remove local erc20 contracts and import npm @openzeppelin/contracts package

--- contracts/Dai.sol
+++ contracts/Dai.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "./ERC20.sol";
+import @openzeppelin/contracts;
 
 contract Dai is ERC20 {
     constructor() ERC20("Dai Stablecoin", "DAI") {}
--- contracts/Rep.sol
+++ contracts/Rep.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import "./ERC20.sol";
 
 contract Rep is ERC20 {
     constructor() ERC20("Rep coin", "REP") {}
