3e2152e66b84de0c5f4aa9fd184c6af4acfc60c7
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-04-13 03:32:41 +0700

    create seedTokenBalance for testing

--- test/dex.js
+++ test/dex.js
@@ -6,9 +6,9 @@ const Rep = artifacts.require("tokens/Rep.sol");
 const Zrx = artifacts.require("tokens/Zrx.sol");
 const Dex = artifacts.require("Dex.sol");
 
-contract("Dex", () => {
+contract("Dex", (accounts) => {
     let dai, bat, rep, zrx;
-
+    const [trader1, trader2] = [accounts[1], accounts[2]];
     const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map(ticker => web3.utils.fromAscii(ticker));
 
     beforeEach( async () => {
@@ -18,6 +18,7 @@ contract("Dex", () => {
             Rep.new(),
             Zrx.new()
         ]));
+        // init Dex
         const dex = await Dex.new();
         await Promise.all([
             dex.addToken(DAI,dai.address),
@@ -25,5 +26,21 @@ contract("Dex", () => {
             dex.addToken(REP,rep.address),
             dex.addToken(ZRX,zrx.address)
         ]);
+        // init seed token balance for testing
+        const amount = web3.utils.toWei(1000);
+        const seedTokenBalance = async (token, trader) => {
+            await token.faucet(trader, amount);
+            await token.approve(
+                dex.address,
+                amount,
+                {from: trader}
+            );
+        };
+        await Promise.all(
+            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader1))
+        );
+        await Promise.all(
+            [dai, bat, rep, zrx].map(token => seedTokenBalance(token, trader2))
+        );
     });
 });
