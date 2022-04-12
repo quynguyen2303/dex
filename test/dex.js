c168a486e65f80e4d49a5e97231a5416c76020cc
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-04-13 03:20:55 +0700

    add dex to test

--- test/dex.js
+++ test/dex.js
@@ -1,10 +1,15 @@
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
 const Dai = artifacts.require("tokens/Dai.sol");
 const Bat = artifacts.require("tokens/Bat.sol");
 const Rep = artifacts.require("tokens/Rep.sol");
 const Zrx = artifacts.require("tokens/Zrx.sol");
+const Dex = artifacts.require("Dex.sol");
 
 contract("Dex", () => {
     let dai, bat, rep, zrx;
+
+    const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].map(ticker => web3.utils.fromAscii(ticker));
 
     beforeEach( async () => {
         ([dai, bat, rep, zrx] = await Promise.all([
@@ -12,7 +17,13 @@ contract("Dex", () => {
             Bat.new(),
             Rep.new(),
             Zrx.new()
-        ])
-        );
+        ]));
+        const dex = await Dex.new();
+        await Promise.all([
+            dex.addToken(DAI,dai.address),
+            dex.addToken(BAT,bat.address),
+            dex.addToken(REP,rep.address),
+            dex.addToken(ZRX,zrx.address)
+        ]);
     });
 });
