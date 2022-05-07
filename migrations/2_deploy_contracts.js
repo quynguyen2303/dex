8c9ca5200e75f023cabd2de09b1dc02bc7fbc5be
Quy Nguyen <nguyenphuquy2303@gmail.com>
2022-05-07 10:19:01 +0700

    Added migration deployment

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,26 @@
+const Dai = artifacts.require("tokens/Dai.sol");
+const Bat = artifacts.require("tokens/Bat.sol");
+const Rep = artifacts.require("tokens/Rep.sol");
+const Zrx = artifacts.require("tokens/Zrx.sol");
+const Dex = artifacts.require("Dex.sol");
+
+const [DAI, BAT, REP, ZRX] = ["DAI", "BAT", "REP", "ZRX"].
+    map(ticker => web3.utils.fromAscii(ticker));
+
+// deploy function
+module.exports = async function(deployer) {
+    await Promise.all(
+        [Dai, Bat, Rep, Zrx, Dex].map(contract => deployer.deploy(contract))
+    );
+
+    const [dai, bat, rep, zrx, dex] = await Promise.all(
+        [Dai, Bat, Rep, Zrx, Dex].map(contract => contract.deployed())
+    ); 
+
+    await Promise.all([
+        dex.addToken(DAI,dai.address),
+        dex.addToken(BAT,bat.address),
+        dex.addToken(REP,rep.address),
+        dex.addToken(ZRX,zrx.address)
+    ]);
+};
